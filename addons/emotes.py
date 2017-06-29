70dcb6d5f06a605a17f32bed3fb35d58d61f32cb
LyricLy <hanson.twentytwo@gmail.com>
2017-06-29 15:44:31 +1200

    *sigh*

--- addons/emotes.py
+++ addons/emotes.py
@@ -15,7 +15,7 @@ class Utility:
     async def emoji(self, emoji_name, image_url):
         """Add an emoji to the server from a URL."""
         image = requests.get(image_url, stream=True)
-        await self.bot.create_custom_emoji(self.bot.server, name=emoji_name, image=image)  
+        await self.bot.create_custom_emoji(self.bot.server, name=emoji_name, image=image.content)  
         await self.bot.say("Successfully added the {} emoji to the server!".format(emoji_name))
         
 def setup(bot):
