36bdcca71b12dc90bb0288a844a808d6edb10cde
LyricLy <hanson.twentytwo@gmail.com>
2017-07-12 16:25:03 +1200

    fixes

--- addons/mod.py
+++ addons/mod.py
@@ -46,9 +46,9 @@ class Moderation:
     @commands.has_permissions(ban_members=True)    
     @commands.command(pass_context=True)
     async def mute(self, ctx, *, member):
-        found_member = get_member(member)
+        found_member = self.bot.get_member(member)
         if not found_member:
-            found_member = get_member_named(member)
+            found_member = self.bot.get_member_named(member)
         if not found_member:
             try:
                 found_member = ctx.message.mentions[0]
@@ -59,6 +59,26 @@ class Moderation:
         else:
             await self.bot.add_roles(found_member, self.bot.muted_role)
             await self.bot.say("Successfully muted user {0.name}#{0.discriminator}!".format(found_member))
+            
+    @commands.has_permissions(ban_members=True)    
+    @commands.command(pass_context=True)
+    async def unmute(self, ctx, *, member):
+        found_member = self.bot.get_member(member)
+        if not found_member:
+            found_member = self.bot.get_member_named(member)
+        if not found_member:
+            try:
+                found_member = ctx.message.mentions[0]
+            except IndexError:
+                pass
+        if not found_member:
+            await self.bot.say("That user could not be found.")
+        else:
+            if "Muted" in [x.name for x in found_member.roles]:
+                await self.bot.remove_roles(found_member, self.bot.muted_role)
+                await self.bot.say("Successfully unmuted user {0.name}#{0.discriminator}!".format(found_member))
+            else:
+                await self.bot.say("That user isn't muted!")
         
 def setup(bot):
     bot.add_cog(Moderation(bot))
